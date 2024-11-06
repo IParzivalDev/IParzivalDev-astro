c1bc4ff863e3002736fbb5d0b5460dde1e6a08c5
IParzivalDev <iparzivalmc@proton.me>
2024-11-05 21:44:33 -0400

    First Commit

---
+++ src/config.ts
@@ -0,0 +1,136 @@
+export const config = {
+  "title":"ParzivalDev",
+  "description":"As a programmer, I'm passionate about creating innovative and efficient solutions using the latest technologies. My experience includes both front-end and back-end development, allowing me to build complete and functional web applications.As a programmer, I'm passionate about creating innovative and e My experience includes both front-end and back-end development, allowing me to build complete and functional web applications.",
+  "image":"/icon.png",
+  "url":"https://iparzivaldev.github.io/IParzivalDev",
+  "color":"#3078ff",
+  "keywords": "ParzivalDev,Junior Full Stack Developer,Programmer,Innovative solutions,Efficient solutions,Front-end development,Back-end development,Complete web applications,Functional web applications,Creative designer, IParzivalDev"
+}
+
+export const sampleCodes = {
+  text: `import random
+
+def totally_random_function_with_no_apparent_functionality():
+  weird_list = [
+    "Fluffy", "Squishy", "Wobbly", "Sassy", "Goofy",
+    "Slippery", "Bouncy", "Grumpy", "Zany", "Fuzzy"
+  ]
+    
+  weird_and_random_list = [
+    "Platypus", "Unicorn", "Pangolin", "Narwhal", "Sloth",
+    "Octopus", "Llama", "Turtle", "Kangaroo", "Ostrich"
+  ]
+  
+  weird_name = random.choice(weird_list) + random.choice(weird_and_random_list)
+  
+  return weird_name`,
+  lang: "python"
+}
+
+export const skills = [
+  {
+    url: "https://python.org/",
+    img: "/skills/python.svg",
+    name: "Python",
+  },
+  {
+    url: "https://developer.mozilla.org/es/docs/Web/HTML",
+    img: "/skills/html.svg",
+    name: "HTML",
+  },
+  {
+    url: "https://developer.mozilla.org/es/docs/Web/CSS",
+    img: "/skills/css.svg",
+    name: "CSS",
+  },
+  {
+    url: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+    img: "/skills/js.svg",
+    name: "Javascript",
+  },
+  {
+    url: "https://www.typescriptlang.org/",
+    img: "/skills/typescript.svg",
+    name: "Typescript",
+  },
+  {
+    url: "https://nodejs.org/",
+    img: "/skills/node.svg",
+    name: "NodeJS",
+  },
+  {
+    url: "https://es.wikipedia.org/wiki/Bash",
+    img: "/skills/bash.svg",
+    name: "Bash",
+  },
+  {
+    url: "https://blockbench.net/",
+    img: "/skills/blockbench.png",
+    name: "Blockbench",
+  },
+  {
+    url: "https://getbootstrap.com/",
+    img: "/skills/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    url: "https://bukkit.org/",
+    img: "/skills/bukkit.png",
+    name: "Bukkit",
+  },
+  {
+    url: "https://go.dev/",
+    img: "/skills/go.png",
+    name: "Golang",
+  },
+  {
+    url: "https://inkscape.org/",
+    img: "/skills/inkscape.svg",
+    name: "Inkscape",
+  },
+  {
+    url: "https://java.com/",
+    img: "/skills/java.png",
+    name: "Java",
+  },
+  {
+    url: "https://es.wikipedia.org/wiki/GNU/Linux",
+    img: "/skills/linux.png",
+    name: "Linux",
+  },
+  {
+    url: "https://nextjs.org/",
+    img: "/skills/next.svg",
+    name: "NextJS",
+  },
+  {
+    url: "https://papermc.io/",
+    img: "/skills/paper.png",
+    name: "PaperMC",
+  },
+  {
+    url: "http://react.dev/",
+    img: "/skills/react.svg",
+    name: "ReactJS",
+  },
+  {
+    url: "https://www.rust-lang.org/",
+    img: "/skills/rust.svg",
+    name: "Rust",
+  },
+  {
+    url: "https://www.spigotmc.org/",
+    img: "/skills/spigot.png",
+    name: "Spigot",
+  },
+  {
+    url: "https://es.wikipedia.org/wiki/SQL",
+    img: "/skills/sql.png",
+    name: "SQL",
+  },
+  {
+    url: "https://tailwindcss.com/",
+    img: "/skills/tailwind.svg",
+    name: "TailwindCSS",
+  },
+]
